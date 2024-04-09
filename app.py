7d05e8d20728dd70657aacedf0dfaaa3b4fbda4f
JuanantPineda <juanantpiama@gmail.com>
2024-04-09 11:52:18 +0200

    Se ha creado el fichero formulario.html y se ha metido en el app.py ademas de crear la imagen en el index.html

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/formulario')
+def formulario():
+    return render_template("formulario.html")
+
 app.run("0.0.0.0",5000,debug=True)
