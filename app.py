3493fd4cb4090fc96098c5f2174870af0a087416
JuanantPineda <juanantpiama@gmail.com>
2024-04-09 12:28:33 +0200

    Se ha creado el formulario ademas del html episodios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, redirect
+from flask import Flask, render_template, abort, redirect,request
 app = Flask(__name__)	
 
 @app.route('/')
@@ -8,5 +8,10 @@ def index():
 @app.route('/formulario')
 def formulario():
     return render_template("formulario.html")
+
+@app.route('/episodios',methods=["POST"])
+def episodios():
+    caracter=request.form.get("letra")
+    return render_template("episodios.html")
 
 app.run("0.0.0.0",5000,debug=True)
